71f77789686cde4694851cc12521a7f6220469f7
qvarnstr0m <mqvarnstrom80@gmail.com>
2023-04-20 11:47:18 +0200

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,68 @@
+﻿namespace AvanceradDotNetLabb3
+{
+    internal class Car
+    {
+        public string Name { get; set; }
+        public int Distance { get; set; }
+        public int Speed { get; set; }
+
+        public static int place = 1;
+
+        public static int totalCars = 0;
+
+        
+
+        public Car(string name)
+        {
+            Name = name;
+            Distance = 0;
+            Speed = 120;
+        }
+
+        public void Race(object raceDistanceObject)
+        {
+            int raceDistance = (int)raceDistanceObject;
+            totalCars++;
+            while (Distance < raceDistance)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(30));
+                HandleRandomEvent();
+                Distance += Speed / 2;
+            }
+            Console.WriteLine($"Car {Name} has crossed the finish line in place {place}!");
+            place++;
+            totalCars--;
+            if (totalCars < 1)
+            {
+                Console.WriteLine($"That was the last car of the race, enter exit to terminate program");
+            }
+        }
+
+        public void HandleRandomEvent()
+        {
+            Random random = new Random();
+            int probability = random.Next(1, 51);
+
+            if (probability == 1)
+            {
+                Console.WriteLine($"Car {Name} runs out of gas, 30 seconds delay.");
+                Thread.Sleep(TimeSpan.FromSeconds(30));
+            }
+            else if (probability <= 2)
+            {
+                Console.WriteLine($"Car {Name} gets a flat tire, 20 seconds delay.");
+                Thread.Sleep(TimeSpan.FromSeconds(20));
+            }
+            else if (probability <= 5)
+            {
+                Console.WriteLine($"Car {Name} hits a bird, 5 seconds delay.");
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+            }
+            else if (probability <= 10)
+            {
+                Console.WriteLine($"Car {Name} gets an engine malfunction, speed is reduced from {Speed} km/h to {Speed - 1} km/h.");
+                Speed -= 1;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AvanceradDotNetLabb3
+{
+    internal class Program
+    {
+        private static List<Car> raceList = new List<Car>();
+        static void Main(string[] args)
+        {
+            Car audi = new Car("Audi");
+            Car bmw = new Car("Bmw");
+
+            raceList.Add(audi);
+            raceList.Add(bmw);
+
+            int raceDistance = 1000;
+
+            Console.WriteLine("The race is starting!");
+            Console.WriteLine($"Car {audi.Name} starts the race");
+            Console.WriteLine($"Car {bmw.Name} starts the race");
+
+            Thread thread1 = new Thread(audi.Race);
+            thread1.Start(raceDistance);
+
+            Thread thread2 = new Thread(bmw.Race);
+            thread2.Start(raceDistance);
+
+            Thread thread3 = new Thread(HandleInput);
+            thread3.Start();
+
+            // Wait for both threads to complete
+            thread1.Join();
+            thread2.Join();
+        }
+
+        private static void HandleInput()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "status")
+                {
+                    foreach (var item in raceList)
+                    {
+                        Console.WriteLine($"{item.Name} has travelled {item.Distance} and is running at a speed of {item.Speed} km/h");
+                    }
+                }
+                else if (input.ToLower() == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
